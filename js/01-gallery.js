aec053f5678b94e475bce6504524c6ea8f36efed
Serg-Babak <babakserg1@gmail.com>
2022-11-19 21:16:59 +0200

    123

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const galeryList = document.querySelector('.gallery');
+const makeGaleryList = galleryItems.map((galleryItem) =>
+`<li> <img class=gallery__image src=${galleryItem.preview} alt=${galleryItem.description} width="354" height="240"> </li>`)
+.join("");
+galeryList.style.listStyle = 'none';
+
+galeryList.insertAdjacentHTML("beforeend", makeGaleryList) ; 
 console.log(galleryItems);
+
+document.querySelector('li').onclick = () => {
+
+	basicLightbox.create(`
+    <li> <img width="1400" height="900" src=${galleryItems.original}></li>
+	`)
+    .show()
+
+}
+
+{/* <div class="gallery__item">
+        <a class="gallery__link" href="${galleryItems.original}">
+    <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        data-source="${galleryItem.original}"
+        alt="${galleryItem.description}"
+        width="354" height="240"
+    />
+        </a>
+</div> */}
+
+
+// const instance = basicLightbox.create(`
+// <div class="gallery__item">
+//     <a class="gallery__link" href="large-image.jpg">
+//     <img
+//         class="gallery__image"
+//         src=${galleryItem.preview}
+//         data-source="large-image.jpg"
+//         alt=${galleryItem.description}
+//     />
+//     </a>
+// </div>
+// `)
+// instance.show()
